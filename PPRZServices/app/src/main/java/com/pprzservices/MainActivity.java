fb6b0c33e0ef3a1116664f8597393db51511f209
Christophe De Wagter <dewagter@gmail.com>
2015-11-11 14:44:32 +0100

    [bebop_start_ap] Button to telnet to the drone, and start paparazzi

--- PPRZServices/app/src/main/java/com/pprzservices/MainActivity.java
+++ PPRZServices/app/src/main/java/com/pprzservices/MainActivity.java
@@ -5,12 +5,24 @@ import com.pprzservices.service.MavLinkService;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.SystemClock;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class MainActivity extends Activity {
 
+	Handler mHandler = null;
+    Thread mThread = null;
 	private static final String TAG = MainActivity.class.getSimpleName();
 	
 	Intent intent;
@@ -19,11 +31,19 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
-        intent = new Intent(this, MavLinkService.class);
+
+		this.mHandler = new Handler();
+		intent = new Intent(this, MavLinkService.class);
         startService(intent);
 
 		Log.d(TAG, "The service is started");
+		Button btnEnter = (Button) findViewById(R.id.bebopStartButton);
+		btnEnter.setOnClickListener(new View.OnClickListener() {
+			public void onClick(View arg0) {
+				bebopStart();
+			}
+		});
+
 	}
 
 	@Override
@@ -59,5 +79,135 @@ public class MainActivity extends Activity {
     public void onDestroy() {
 		super.onDestroy();
 		stopService(intent);
-    }	
+    }
+
+
+	public void bebopStart() {
+        if(mThread != null)
+        {
+            mThread.stop();
+            mThread = null;
+        }
+
+        mThread = new Thread(new TelnetClient());
+        mThread.start();
+	}
+
+    public void showToast(final String toast)
+    {
+        runOnUiThread(new Runnable() {
+            public void run() {
+                Toast.makeText(MainActivity.this, toast, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+
+    public class TelnetClient implements Runnable
+    {
+        Handler mHandler = null;
+        Socket mSocket = null;
+
+        public void send(String strCommand)
+        {
+            try
+            {
+                OutputStream streamOutput = mSocket.getOutputStream();
+
+                try
+                {
+                    byte[] arrayOutput = strCommand.getBytes("ASCII");
+                    int nLen = arrayOutput.length;
+                    streamOutput.write(arrayOutput, 0, nLen);
+                }
+                catch (Exception e0)
+                {}
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+        }
+/*
+        public void read()
+        {
+            try
+            {
+                InputStream streamInput = mSocket.getInputStream();
+
+                byte[] arrayOfByte = new byte[10000];
+                int j = 0;
+                try
+                {
+                    int i = arrayOfByte.length;
+                    j = streamInput.read(arrayOfByte, 0, i);
+                    if (j == -1)
+                    {
+                        throw new Exception("Error while reading socket.");
+                    }
+                }
+                catch (Exception e0)
+                {
+                    try
+                    {
+                        mSocket.close();
+                    }
+                    catch (IOException e1)
+                    {
+                        e1.printStackTrace();
+                    }
+                    mSocket = null;
+                }
+            }
+            catch (IOException e1)
+            {
+                e1.printStackTrace();
+            }
+        }
+*/
+
+
+        public void run()
+        {
+            try
+            {
+                Socket socket = new Socket("192.168.42.1", 23);
+                mSocket = socket;
+
+                SystemClock.sleep(100);
+                showToast("Connected");
+
+                this.send("root\n");
+
+                SystemClock.sleep(200);
+                this.send("\n");
+
+                SystemClock.sleep(500);
+                this.send("killall -9 dragon-prog.elf\n");
+                this.send("killall -9 program.elf\n");
+
+                SystemClock.sleep(1000);
+                this.send("cd /data/ftp/ap/\n");
+
+                SystemClock.sleep(500);
+                this.send("./ap.ef > /dev/null 2>&1 &\n");
+
+                socket.close();
+                mSocket = null;
+            }
+            catch (Exception e0)
+            {
+                String strException = e0.getMessage();
+
+                if(strException == null)
+                    strException = "Connection closed";
+                else
+                    strException = "Cannot connect to the server:\r\n" + strException;
+
+                showToast(strException);
+      }
+        }
+    }
+
+
 }
